5e999b230ebd163d1c896361c67a1ec15bf86270
magbee <david.magbee@me.com>
2020-02-25 10:18:11 -0500

    comment cleanup

--- blurbs/src/App.js
+++ blurbs/src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import './App.css';
 
 import Main from './Main';
-import Mic from './Mic';
-import Contact from './Contact';
-import About from './About';
-import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <Link to='/About'>About</Link> */}
-        {/* <Link to='/'>Mic</Link> */}
-      {/* </header> */}
       <main>
         <Router>
-        {/* <input type='file' accept='audio/*' capture id='microphone' /> */}
-          {/* <Route path='/' component={Mic}></Route>
-          <Route path='/About' exact component={About}></Route> */}
           <Main />
-          {/* <Welcome /> */}
-          {/* <Mic /> */}
-          {/* <Contact /> */}
-          {/* <About /> */}
         </Router>
       </main>
     </div>
